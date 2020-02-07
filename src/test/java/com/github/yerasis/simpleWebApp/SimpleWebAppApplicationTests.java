44f60da69b157cd43d7c41e5e346c50407911c6d
yeras-is <maihanov95@gmail.com>
2020-02-07 10:29:38 +0600

    Initial commit

---
+++ src/test/java/com/github/yerasis/simpleWebApp/SimpleWebAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.yerasis.simpleWebApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleWebAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
