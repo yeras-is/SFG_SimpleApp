83e40ac3553d07a27076f0a99eb1d7f4553cde9e
yeras-is <maihanov95@gmail.com>
2020-02-18 00:01:56 +0600

    Authors Controller and Page

--- src/main/java/com/github/yerasis/simpleWebApp/controller/AuthorController.java
+++ src/main/java/com/github/yerasis/simpleWebApp/controller/AuthorController.java
@@ -6,6 +6,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+@Controller
 @RequestMapping("/authors")
 public class AuthorController {
 
